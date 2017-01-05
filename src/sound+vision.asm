48b15e7f26839402fd5e08f5ca8285ffeb8bb3e0
rhargreaves <robert@roberthargreaves.net>
2017-01-05 23:48:13 +0000

    Seperate operands from operators

--- src/sound+vision.asm
+++ src/sound+vision.asm
@@ -5,17 +5,17 @@ org 8000h
 ; 7 6 5 4 3 2 1 0
 
 main		di			; disable maskable interrupts
-		ld c,0ffh
-snd_loop	ld a,01h
-		out (0feh),a		; 0 to SPK, 1 to BORDER
-		ld b,0c0h
-delay1		djnz (delay1)
-		ld a,013h
-		out (0feh),a		; 1 to SPK, 3 to BORDER
-		ld b,0c0h
-delay2		djnz (delay2)
-		dec c			; decrement main loop
-		jr nz,(snd_loop)
+		ld	c,0ffh
+snd_loop	ld	a,01h
+		out	(0feh),a	; 0 to SPK, 1 to BORDER
+		ld	b,0c0h
+delay1		djnz	(delay1)
+		ld	a,013h
+		out	(0feh),a	; 1 to SPK, 3 to BORDER
+		ld	b,0c0h
+delay2		djnz	(delay2)
+		dec	c		; decrement main loop
+		jr	nz,(snd_loop)
 		ei			; enable maskable interrupts
 		ret
 
